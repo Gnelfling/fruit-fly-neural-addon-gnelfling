602fa4aced21160c3b6c18770615d529537a227c
gabriel <kleincolombo@gmail.com>
2026-09-16 20:52:45 -0300

    Add shared entity definition for the FlyWire fly addon.

---
+++ lua/entities/npc_flywire_fly/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "FlyWire Fly"
+ENT.Author = "Copilot"
+ENT.Category = "NPCs"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+
+-- Placeholder model for easy testing; replace with a custom fly mesh later.
+ENT.Model = "models/props_junk/PopCan01a.mdl"
